b1d4f20f746d3b13acf36a1dcc0c07e206dd5061
ArtisDev <artisdev@proton.me>
2022-10-05 14:22:54 +0300

    Add Main component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Main from './components/Main';
 import Footer from './components/Footer';
 
 class App extends React.Component {
@@ -18,9 +19,9 @@ class App extends React.Component {
     const { cv } = this.state;
 
     return (
-      <div className="flex h-screen flex-col">
+      <div className="flex h-screen flex-col bg-slate-100">
         <Header />
-        <p className="grow py-4 text-center">{cv.name}</p>
+        <Main cv={cv} />
         <Footer />
       </div>
     );
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+function Main() {
+  return (
+    <main className="flex grow flex-col py-8 px-4">
+      <button
+        className="place-self-center rounded bg-slate-700 py-2 px-3 text-white duration-200 hover:bg-slate-600"
+        type="button"
+      >
+        PREVIEW
+      </button>
+      <div className="container mx-auto my-8 max-w-3xl bg-white py-12 px-28">
+        CV builder
+      </div>
+    </main>
+  );
+}
+
+export default Main;
