d5d3002193b6f434efda95ed866be3f06a52eb8d
ArtisDev <artisdev@proton.me>
2022-10-05 14:26:24 +0300

    Extract Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+function Button() {
+  return (
+    <button
+      className="place-self-center rounded bg-slate-700 py-2 px-3 text-white duration-200 hover:bg-slate-600"
+      type="button"
+    >
+      PREVIEW
+    </button>
+  );
+}
+
+export default Button;
