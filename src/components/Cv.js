3ba43d5c3c87b667175a3170c62d253c1b39dc27
ArtisDev <artisdev@proton.me>
2022-10-05 14:33:26 +0300

    Extract Cv component

---
+++ src/components/Cv.js
@@ -0,0 +1,9 @@
+function Cv() {
+  return (
+    <div className="container mx-auto my-8 max-w-3xl bg-white py-12 px-28">
+      CV builder
+    </div>
+  );
+}
+
+export default Cv;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,11 @@
 import Button from './Button';
+import Cv from './Cv';
 
 function Main() {
   return (
     <main className="flex grow flex-col py-8 px-4">
       <Button />
-      <div className="container mx-auto my-8 max-w-3xl bg-white py-12 px-28">
-        CV builder
-      </div>
+      <Cv />
     </main>
   );
 }
