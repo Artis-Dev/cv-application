bd36f6468916a79073f036e74ab6783367c7b790
ArtisDev <artisdev@proton.me>
2022-10-05 17:55:07 +0300

    Change App component from class to function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import React from 'react';
-
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cv: {
-        name: 'Hello World',
-      },
-    };
-  }
-
-  render() {
-    const { cv } = this.state;
-
-    return (
-      <div className="flex h-screen flex-col bg-slate-100">
-        <Header />
-        <Main cv={cv} />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="flex h-screen flex-col bg-slate-100">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
